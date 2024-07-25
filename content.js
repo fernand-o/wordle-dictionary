249cc1493cab322640008ef2d7047335f49169a4
Fernando Almeida <fernandoprsbr@gmail.com>
2024-07-25 16:55:21 -0300

    Add logic to detect when game starts

--- content.js
+++ content.js
@@ -1,15 +1,20 @@
-function words() {
-  const validStates = ['present', 'correct', 'absent'];
+const GUESSED_STATES = ['present', 'correct', 'absent'];
 
+function tiles() {
+  return [...document.querySelectorAll('div[aria-roledescription="tile"]')]
+}
+
+function readWords() {
   return rows().map(row => {
-    if (row.every(letter => validStates.includes(letter.getAttribute('data-state')))) {
+    if (row.every(letter => GUESSED_STATES.includes(letter.getAttribute('data-state')))) {
       return row.map(letter => letter.innerText).join('');
     }
   }).filter(Boolean);
 }
 
 function displayLastWordMeaning() {
-  const lastWord = words()[words.length - 1];
+  const words = readWords();
+  const lastWord = words[words.length - 1];
 
   setTimeout(() => {
     console.log("fun fact: " + lastWord);
@@ -25,8 +30,7 @@ function chunkArray(array, chunkSize) {
 }
 
 function rows() {
-  const letterElements = [...document.querySelectorAll('div[aria-roledescription="tile"]')];
-  return chunkArray(letterElements, 5);
+  return chunkArray(tiles(), 5);
 }
 
 function isLastLetter(element) {
@@ -40,23 +44,34 @@ function handleMutation(mutationsList) {
     if (mutation.type === 'attributes' && mutation.attributeName === 'data-state') {
       const element = mutation.target;
       const newState = element.getAttribute('data-state');
-      const validGuessedStates = ['present', 'correct', 'absent'];
 
       console.log(`Element's data-state changed to: ${newState}, with letter: ${element.innerText}`);
       
-      if (validGuessedStates.includes(newState) && isLastLetter(element)) {
-        console.log('last letter was guessed');
+      if (GUESSED_STATES.includes(newState) && isLastLetter(element)) {
         displayLastWordMeaning();
       }
     }
   }
 }
 
-const observer = new MutationObserver(handleMutation);
-const elementsToObserve = document.querySelectorAll('[data-state]');
+function startGame() {
+  console.log('Game is starting...');
 
-elementsToObserve.forEach(element => {
-  observer.observe(element, { attributes: true });
-});
+  const observer = new MutationObserver(handleMutation);
 
+  tiles().forEach(element => {
+    observer.observe(element, { attributes: true });
+  });
+}
 
+function observeGameStart() {
+  const game = document.querySelector('[id="wordle-app-game"]');
+  
+  if (game) {
+    startGame();
+  } else {
+    setTimeout(observeGameStart, 500);
+  }
+}
+
+observeGameStart();
